985cd8fc73cf05b67d18bcfa3d89540409f548fe
markzazdravnykh <148939807+markzazdravnykh@users.noreply.github.com>
2025-03-10 17:58:44 +0100

    Add files via upload

---
+++ zelva.py
@@ -0,0 +1,20 @@
+from turtle import forward, exitonclick, left, right
+
+left(45)
+forward(140)
+left(90)
+forward(71)
+left(90)
+forward(71)
+left(135)
+forward(100)
+right(90)
+forward(100)
+right(135)
+forward(140)
+left(135)
+forward(100)
+left(90)
+forward(100)
+
+exitonclick()
