1b8df33ebb2c4c194732cf7d24c83b72f492a9e7
markzazdravnykh <mark.zazdravnykh.s@gyarab.cz>
2025-03-28 09:51:28 +0100

    updt

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Deck
+class DeckAdmin(admin.ModelAdmin):
+        list_display = [ "id", "name", "year"]
 # Register your models here.
+admin.site.register(Deck, DeckAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
-# Create your models here.
+Class Deck(models.Deck):
+    name = models.CharField(max_length=300)
+    year = models.IntegerField(blank=True, null=False)
+    
+    def __str__(self):
+        return f"Deck <(self):
+            
